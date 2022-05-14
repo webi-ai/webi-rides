f8a7fd9ff544ce5007a28f09d5a5c21daadf0630
kelsey <enktal@gmail.com>
2022-05-14 15:26:30 -0700

    first unit test

--- test/ride.js
+++ test/ride.js
@@ -1,8 +1,12 @@
 const RideManager = artifacts.require("./RideManager.sol");
 
 contract("RideManager", accounts => {
-  it("...should set return drivers available.", async () => {
+  it("...should set return drivers available, which will be false", async () => {
     const rideInstance = await RideManager.deployed();
-    const rideInfo = await rideInstance.returnDriversAvailable();
+    const ridesAvailable = await rideInstance.returnDriversAvailable();
+    //console.log(ridesAvailable);
+    assert.equal(ridesAvailable > 0, false);
   });
+
 });
+
