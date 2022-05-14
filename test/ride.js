ef799b32be656d99e10754dfc1285bd5baae4053
kelsey <enktal@gmail.com>
2022-05-14 14:54:53 -0700

    test update

--- test/ride.js
+++ test/ride.js
@@ -2,11 +2,13 @@ const Ride = artifacts.require("../contracts/Ride.sol");
 
 contract("Ride", accounts => {
   it("...should set rider confirmation to true.", async () => {
-    const rideInfo = await Ride.getRideInfo();
+    const rideInstance = await Ride.deployed();
+    const rideInfo = await rideInstance.getRideInfo();
     assert.equal(rideInfo.confirmedByRider, false, "Rider confirmation is not false.");
 
     // Set Rider Confirmation to true
-    await Ride.updateRiderConfirmation.set("True");
-    assert.equal(rideInfo.confirmedByRider, true, "Rider confirmation is not true.");
+    await rideInstance.updateRiderConfirmation.set("True");
+    const rideInfo2 = await rideInstance.getRideInfo();
+    assert.equal(rideInfo2.confirmedByRider, true, "Rider confirmation is not true.");
   });
 });
