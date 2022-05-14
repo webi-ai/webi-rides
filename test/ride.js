ecccdaf34f1fb013299eab690802831ed5f9ad33
kelsey <enktal@gmail.com>
2022-05-14 15:49:28 -0700

    almost

--- test/ride.js
+++ test/ride.js
@@ -7,6 +7,16 @@ contract("RideManager", accounts => {
     //console.log(ridesAvailable);
     assert.equal(ridesAvailable > 0, false);
   });
-
+  it("...should register a rider", async () => {
+    const rideInstance = await RideManager.deployed();
+    const result = await rideInstance.registerRider(
+        web3.utils.fromAscii("Rider1"),
+        web3.utils.fromAscii("1234567890"),
+        web3.utils.fromAscii("rider1@webi.io"),
+        web3.utils.fromAscii("rider"),
+        "0xa09f74450A9fc56238d111f3550a157edBc2E07E"
+        
+    );
+    console.log(result);
+  });
 });
-
