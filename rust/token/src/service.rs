dc4b34c37493b81e86f1fd3e9f51f3f33831c6e4
kelsey <enktal@gmail.com>
2022-05-17 22:07:46 -0700

    make structs

--- rust/token/src/service.rs
+++ rust/token/src/service.rs
@@ -7,46 +7,21 @@
 * Description   :  Token Service Contracts
 */
 
-use candid::{candid_method, CandidType, Deserialize, Int, Nat};
-use cap_sdk::{handshake, insert, Event, IndefiniteEvent, TypedEvent};
-use cap_std::dip20::cap::DIP20Details;
-use cap_std::dip20::{Operation, TransactionStatus, TxRecord};
 use ic_cdk_macros::*;
 use ic_kit::{ic, Principal};
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::collections::VecDeque;
-use std::convert::Into;
-use std::iter::FromIterator;
-use std::string::String;
 
-#[derive(CandidType, Default, Deserialize, Clone)]
-pub struct TxLog {
-  pub ie_records: VecDeque<IndefiniteEvent>,
+
+enum Roles {
+    rider,
+    driver
 }
 
-#[derive(Deserialize, CandidType, Clone, Debug)]
-struct StatsData {
-  logo: String,
-  name: String,
-  symbol: String,
-  decimals: u8,
-  total_supply: Nat,
-  owner: Principal,
-  fee: Nat,
-  fee_to: Principal,
-  history_size: usize,
-  deploy_time: u64,
+pub struct Rider {
+    name: String,
+    contact: u32,
+    email: String,
+    role: Roles,
+    addresses: Option<Vec<Principal>>,
+    address: Principal,
 }
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, CandidType, Clone, Debug)]
-struct TokenInfo {
-  metadata: Metadata,
-  feeTo: Principal,
-  // status info
-  historySize: usize,
-  deployTime: u64,
-  holderNumber: usize,
-  cycles: u64,
-}
