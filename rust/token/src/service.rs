bbe9fdab911fcad4c0cc643f514062b7217c98cf
kelsey <enktal@gmail.com>
2022-05-18 13:47:57 -0700

    driver struct

--- rust/token/src/service.rs
+++ rust/token/src/service.rs
@@ -10,10 +10,16 @@
 use ic_cdk_macros::*;
 use ic_kit::{ic, Principal};
 
+array<Principal> Drivers;
 
 enum Roles {
     rider,
     driver
+}
+
+enum CurrentStatus {
+    active,
+    inactive
 }
 
 pub struct Rider {
@@ -25,3 +31,21 @@ pub struct Rider {
     address: Principal,
 }
 
+pub struct Driver {
+    name: String,
+    contact: u32,
+    email: String,
+    role: Roles,
+    vehicleplatenumber: String,
+    vehicleseatnumber: String,
+    vehiclemake: String,
+    vehiclemodel: String,
+    vehiclecolor: String,
+    vehicletype: String,
+    vehicleyear: String,
+    rating: Float,
+    role: Roles,
+    currentstatus: CurrentStatus,
+    addresses: Option<Vec<Principal>>,
+    address: Principal,
+}
