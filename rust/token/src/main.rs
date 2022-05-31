adc37c11d6580a0448f5b2bad0a38920262345ec
kelsey <enktal@gmail.com>
2022-05-30 23:53:15 -0700

    search by field

--- rust/token/src/main.rs
+++ rust/token/src/main.rs
@@ -32,7 +32,7 @@ enum CurrentStatus {
 #[derive(Clone, Debug, CandidType, Deserialize)]
 struct Rider {
     pub name: String,
-    pub contact: u32,
+    pub contact: u64,
     pub email: String,
     pub role: String,
     pub addresses: Option<Vec<Principal>>,
@@ -42,7 +42,7 @@ struct Rider {
 #[derive(PartialEq, Clone, Debug, CandidType, Deserialize)]
 struct Driver {
     pub name: String,
-    pub contact: u32,
+    pub contact: u64,
     pub email: String,
     pub role: String,
     pub vehicleplatenumber: String,
@@ -320,6 +320,162 @@ fn search_driver_by_name(driver_name: String) -> Option<Driver> {
         None
     })
 }
+
+// search for driver by contect and return the driver
+#[query]
+fn search_driver_by_contact(contact: u64) -> Option<Driver> {
+    DRIVER_STORE.with(|driver_store| {
+        for driver in driver_store.borrow().iter() {
+            if driver.contact == contact {
+                return Some(driver.clone());
+            }
+        }
+        None
+    })
+}
+
+// test search for driver_by_contact
+#[test]
+fn test_search_driver_by_contact() {
+    let driver = Driver {
+        name: "Kelsey".to_string(),
+        contact: 1234567890,
+        email: "test@email.com".to_string(),
+        role: "driver".to_string(),
+        vehicleplatenumber: "ABC123".to_string(),
+        vehicleseatnumber: "1".to_string(),
+        vehiclemake: "Toyota".to_string(),
+        vehiclemodel: "Corolla".to_string(),
+        vehiclecolor: "Black".to_string(),
+        vehicletype: "SUV".to_string(),
+        vehicleyear: "2020".to_string(),
+        rating: 0.0,
+        currentstatus: CurrentStatus::Active,
+        addresses: None,
+        address: Principal::from_text("cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae").unwrap(),
+    };
+    register_driver(driver);
+    assert_eq!(get_drivers().len(), 1);
+    //check the data was written to the store
+    assert_eq!(get_drivers()[0].name, "Kelsey");
+    assert_eq!(search_driver_by_contact(1234567890).unwrap().name, "Kelsey");
+}
+
+
+//search for driver by any field and return the driver  
+#[query]
+fn search_driver_by_field(_field: String, value: String) -> Option<Driver> {
+    DRIVER_STORE.with(|driver_store| {
+        for driver in driver_store.borrow().iter() {
+            if _field == "name" {
+                if driver.name == value {
+                    return Some(driver.clone());
+                }
+            } else if _field == "contact" {
+                if driver.contact == value.parse::<u64>().unwrap() {
+                    return Some(driver.clone());
+                }
+            } else if _field == "email" {
+                if driver.email == value {
+                    return Some(driver.clone());
+                }
+            } else if _field == "role" {
+                if driver.role == value {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehicleplatenumber" {
+                if driver.vehicleplatenumber == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehicleseatnumber" {
+                if driver.vehicleseatnumber == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehiclemake" {
+                if driver.vehiclemake == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehiclemodel" {
+                if driver.vehiclemodel == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehiclecolor" {
+                if driver.vehiclecolor == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehicletype" {
+                if driver.vehicletype == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "vehicleyear" {
+                if driver.vehicleyear == value
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "rating" {
+                if driver.rating == value.parse::<f64>().unwrap()
+                {
+                    return Some(driver.clone());
+                }
+            } else if _field == "address" {
+                if driver.address == value.parse::<Principal>().unwrap()
+                {
+                    return Some(driver.clone());
+                }
+            }
+        }
+
+        None
+    })
+}
+
+//test search for driver by field
+#[test]
+fn test_search_driver_by_field() {
+    let driver = Driver {
+        name: "Kelsey".to_string(),
+        contact: 1234567890,
+        email: "test@email.com".to_string(),
+        role: "driver".to_string(),
+        vehicleplatenumber: "ABC123".to_string(),
+        vehicleseatnumber: "1".to_string(),
+        vehiclemake: "Toyota".to_string(),
+        vehiclemodel: "Corolla".to_string(),
+        vehiclecolor: "Black".to_string(),
+        vehicletype: "SUV".to_string(),
+        vehicleyear: "2020".to_string(),
+        rating: 0.0,
+        currentstatus: CurrentStatus::Active,
+        addresses: None,
+        address: Principal::from_text("cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae").unwrap(),
+    };
+    register_driver(driver);
+    assert_eq!(get_drivers().len(), 1);
+    //check the data was written to the store
+    assert_eq!(get_drivers()[0].name, "Kelsey");
+    assert_eq!(search_driver_by_field("name".to_string(), "Kelsey".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("contact".to_string(), "1234567890".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("email".to_string(), "test@email.com".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("role".to_string(), "driver".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehicleplatenumber".to_string(), "ABC123".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehicleseatnumber".to_string(), "1".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehiclemake".to_string(), "Toyota".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehiclemodel".to_string(), "Corolla".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehiclecolor".to_string(), "Black".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehicletype".to_string(), "SUV".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("vehicleyear".to_string(), "2020".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("rating".to_string(), "0.0".to_string()).unwrap().name, "Kelsey");
+    assert_eq!(search_driver_by_field("address".to_string(), "cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae".to_string()).unwrap().name, "Kelsey");
+
+
+}
+
 
 //test search for driver by name and return the driver
 #[test]
