21a3368bde6d71e6c5872f3071943aa8266b5c71
kelsey <enktal@gmail.com>
2022-06-14 19:01:32 -0700

    update IDL

--- scripts/agent.js
+++ scripts/agent.js
@@ -29,87 +29,111 @@ const agent = new HttpAgent({
 
 //idl factory generated with the following command:
 //didc bind token.did --target js
- const idlFactory = ({ IDL }) => {
-  const Profile_2 = IDL.Record({
-    'name' : IDL.Text,
-    'description' : IDL.Text,
-    'keywords' : IDL.Vec(IDL.Text),
+export const idlFactory = ({ IDL }) => {
+  const Profile = IDL.Record({
+    'name': IDL.Text,
+    'description': IDL.Text,
+    'keywords': IDL.Vec(IDL.Text),
   });
-  const Driver_2 = IDL.Record({
-    'contact' : IDL.Text,
-    'vehiclemake' : IDL.Text,
-    'vehiclecolor' : IDL.Text,
-    'vehicletype' : IDL.Text,
-    'vehicleyear' : IDL.Text,
-    'vehicleplatenumber' : IDL.Text,
-    'name' : IDL.Text,
-    'role' : IDL.Text,
-    'email' : IDL.Text,
-    'address' : IDL.Principal,
-    'vehicleseatnumber' : IDL.Text,
-    'currentstatus' : IDL.Variant({
-      'Inactive' : IDL.Null,
-      'Active' : IDL.Null,
-    }),
-    'rating' : IDL.Float64,
-    'vehiclemodel' : IDL.Text,
+  const CurrentStatus = IDL.Variant({
+    'Inactive': IDL.Null,
+    'Active': IDL.Null,
   });
-  const Driver = Driver_2;
-  const Rider_2 = IDL.Record({
-    'contact' : IDL.Text,
-    'name' : IDL.Text,
-    'role' : IDL.Text,
-    'email' : IDL.Text,
-    'address' : IDL.Principal,
+  const Driver = IDL.Record({
+    'contact': IDL.Text,
+    'vehiclemake': IDL.Text,
+    'vehiclecolor': IDL.Text,
+    'vehicletype': IDL.Text,
+    'vehicleyear': IDL.Text,
+    'vehicleplatenumber': IDL.Text,
+    'name': IDL.Text,
+    'role': IDL.Text,
+    'email': IDL.Text,
+    'address': IDL.Text,
+    'vehicleseatnumber': IDL.Text,
+    'currentstatus': CurrentStatus,
+    'rating': IDL.Float64,
+    'vehiclemodel': IDL.Text,
   });
-  const Rider = Rider_2;
-  const Ride_2 = IDL.Record({
-    'status' : IDL.Variant({
-      'Active' : IDL.Null,
-      'Cancelled' : IDL.Null,
-      'Completed' : IDL.Null,
-    }),
-    'dropoff' : IDL.Text,
-    'rideid' : IDL.Text,
-    'driverconfirmation' : IDL.Text,
-    'riderrating' : IDL.Float64,
-    'pickup' : IDL.Text,
-    'riderfeedback' : IDL.Text,
-    'timestamp' : IDL.Text,
-    'driverfeedback' : IDL.Text,
-    'rating' : IDL.Float64,
-    'riderconfirmation' : IDL.Text,
-    'driver' : Driver,
-    'rider' : Rider,
-    'driverrating' : IDL.Float64,
+  const Rider = IDL.Record({
+    'contact': IDL.Text,
+    'name': IDL.Text,
+    'role': IDL.Text,
+    'email': IDL.Text,
+    'address': IDL.Text,
   });
-  const Tokens = IDL.Record({ 'e8s' : IDL.Nat64 });
-  const TransferArgs = IDL.Record({
-    'to_principal' : IDL.Principal,
-    'to_subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
-    'amount' : Tokens,
+  const RideStatus = IDL.Variant({
+    'Active': IDL.Null,
+    'Cancelled': IDL.Null,
+    'Completed': IDL.Null,
   });
-  const Memo = IDL.Nat64;
-  const TransferResult = IDL.Variant({ 'Ok' : Memo, 'Err' : IDL.Text });
+  const Ride = IDL.Record({
+    'status': RideStatus,
+    'dropoff': IDL.Text,
+    'rideid': IDL.Text,
+    'driverconfirmation': IDL.Text,
+    'riderrating': IDL.Float64,
+    'pickup': IDL.Text,
+    'riderfeedback': IDL.Text,
+    'timestamp': IDL.Text,
+    'driverfeedback': IDL.Text,
+    'rating': IDL.Float64,
+    'riderconfirmation': IDL.Text,
+    'driver': Driver,
+    'rider': Rider,
+    'driverrating': IDL.Float64,
+  });
   return IDL.Service({
-    'get' : IDL.Func([IDL.Text], [Profile_2], ['query']),
-    'getRides' : IDL.Func([], [IDL.Vec(Ride_2)], ['query']),
-    'getSelf' : IDL.Func([], [Profile_2], ['query']),
-    'get_driver' : IDL.Func([IDL.Text], [Driver_2], ['query']),
-    'get_drivers' : IDL.Func([], [IDL.Vec(Driver_2)], ['query']),
-    'get_ride' : IDL.Func([IDL.Text], [Ride_2], ['query']),
-    'get_rider' : IDL.Func([IDL.Text], [Rider_2], ['query']),
-    'get_riders' : IDL.Func([], [IDL.Vec(Rider_2)], ['query']),
-    'registerDriver' : IDL.Func([Driver_2], [], []),
-    'registerRide' : IDL.Func([Ride_2], [], []),
-    'registerRider' : IDL.Func([Rider_2], [], []),
-    'search' : IDL.Func([IDL.Text], [IDL.Opt(Profile_2)], ['query']),
-    'transfer' : IDL.Func([TransferArgs], [TransferResult], []),
-    'update' : IDL.Func([Profile_2], [], []),
+    'get': IDL.Func([IDL.Text], [Profile], ['query']),
+    'get_drivers': IDL.Func([], [IDL.Vec(Driver)], ['query']),
+    'get_riders': IDL.Func([], [IDL.Vec(Rider)], ['query']),
+    'get_rides': IDL.Func([], [IDL.Vec(Ride)], ['query']),
+    'get_self': IDL.Func([], [Profile], ['query']),
+    'register_driver': IDL.Func([Driver], [], []),
+    'register_ride': IDL.Func([Ride], [], ['query']),
+    'register_rider': IDL.Func([Rider], [], []),
+    'search_driver_by_address': IDL.Func(
+      [IDL.Text],
+      [IDL.Opt(Driver)],
+      ['query'],
+    ),
+    'search_driver_by_contact': IDL.Func(
+      [IDL.Text],
+      [IDL.Opt(Driver)],
+      ['query'],
+    ),
+    'search_driver_by_field': IDL.Func(
+      [IDL.Text, IDL.Text],
+      [IDL.Opt(Driver)],
+      ['query'],
+    ),
+    'search_driver_by_name': IDL.Func(
+      [IDL.Text],
+      [IDL.Opt(Driver)],
+      ['query'],
+    ),
+    'search_ride_by_field': IDL.Func(
+      [IDL.Text, IDL.Text],
+      [IDL.Opt(Ride)],
+      ['query'],
+    ),
+    'search_ride_by_id': IDL.Func([IDL.Text], [IDL.Opt(Ride)], ['query']),
+    'search_rider_by_address': IDL.Func(
+      [IDL.Text],
+      [IDL.Opt(Rider)],
+      ['query'],
+    ),
+    'search_rider_by_field': IDL.Func(
+      [IDL.Text, IDL.Text],
+      [IDL.Opt(Rider)],
+      ['query'],
+    ),
+    'update': IDL.Func([Profile], [], []),
+    'update_driver_rating': IDL.Func([IDL.Text, IDL.Float64], [], []),
+    'update_driver_status': IDL.Func([IDL.Text, CurrentStatus], [], ['query']),
   });
 };
- const init = ({ IDL }) => { return []; };
-
+export const init = ({ IDL }) => { return []; };
 
 //fetch keys, this is not needed in production
 if (true) {
@@ -166,7 +190,7 @@ record_driver_insert = {
   //Float64 becomes a js float
   rating: 4.5,
   //enum becomes variant like this
-  currentstatus: {Active: null},
+  currentstatus: { Active: null },
   address: Principal.Principal.fromText("cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae"),
 }
 
@@ -188,7 +212,7 @@ record_ride_insert = {
   timestamp: "2020-01-01T00:00:00.000Z",
   pickup: "san francisco",
   dropoff: "san jose",
-  status: {Active: null},
+  status: { Active: null },
   rating: 4.5,
   driverrating: 4.5,
   riderrating: 4.5,
@@ -196,14 +220,14 @@ record_ride_insert = {
   riderfeedback: "good rider",
   riderconfirmation: "yes",
   driverconfirmation: "yes",
-  rider : {
+  rider: {
     contact: "1234567890",
     name: "Kelsey",
     email: "test@email.com",
     role: "rider",
     address: Principal.Principal.fromText("cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae"),
   },
-  driver : {
+  driver: {
     contact: "1234567890",
     name: "Kelsey",
     email: "test@email.com",
@@ -218,7 +242,7 @@ record_ride_insert = {
     //Float64 becomes a js float
     rating: 4.5,
     //enum becomes variant like this
-    currentstatus: {Active: null},
+    currentstatus: { Active: null },
     address: Principal.Principal.fromText("cjr37-nxx7a-keiqq-efh5n-v47nd-ceddb-2c6hg-aseen-h66ih-so563-hae"),
   },
 }
